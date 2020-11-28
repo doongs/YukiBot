43bc409292ed28fcd00694013c7d6f921f97affc
Gabriel Doon <doongs@dukes.jmu.edu>
2020-11-28 10:56:12 -0500

    init mangadex

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 const Discord = require('discord.js');
+const Mangadex = require('mangadex-full-api');
+
+Mangadex.agent.login("mfa", "mangadex-full-api", false).then(async () => {
+
+  var manga = new Mangadex.Manga();
+  await manga.fill(6770);
+  //console.log(`${manga.title} by ${manga.authors.join(", ")}`);
+  
+  let chapterId = manga.chapters[0].id;
+
+  // New v4.0.0 Method
+  let chapter = await Mangadex.Chapter.get(chapterId);
+  console.log(`@manga updates\nHey everyone, chapter ${chapter.chapter} is out now from ${chapter.groups[0].title}!\n${chapter.url}`);
+});
+
 // Importing this allows you to access the environment variables of the running node process
+/*
 require('dotenv').config();
 
 const client = new Discord.Client();
@@ -17,3 +33,4 @@ client.on('message', msg => {
 // Here you can login the bot. It automatically attempts to login the bot
 // with the environment variable you set for your bot token ("DISCORD_TOKEN")
 client.login();
+*/
